58e896cee210d375a58539cbe311e8f184699857
JakeFC <jacobchavera@yahoo.com>
2021-09-30 07:09:37 -0700

    Task 7

--- 0x00-unity-assets_ui/Assets/Scripts/CameraController.cs
+++ 0x00-unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -10,11 +10,17 @@ public class CameraController : MonoBehaviour
 	private float yaw = 0f;
 	private float pitch = 0f;
 
+	public bool isInverted = false;
+
     // Rotates the Pivot object and its Main Camera child based on mouse movement.
     void Update()
     {
 		yaw += speedH * Input.GetAxis("Mouse X");
-		pitch -= speedV * Input.GetAxis("Mouse Y");
+
+		if (isInverted)
+			pitch += speedV * Input.GetAxis("Mouse Y");
+		else
+			pitch -= speedV * Input.GetAxis("Mouse Y");
 
 		transform.eulerAngles = new Vector3(pitch, yaw, 0);
     }
