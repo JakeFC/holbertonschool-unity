2da8de29b067686637e45da484a33f056ceddd33
JakeFC <jacobchavera@yahoo.com>
2021-09-30 07:07:22 -0700

    Task 6

--- 0x00-unity-assets_ui/Assets/Scripts/PauseMenu.cs
+++ 0x00-unity-assets_ui/Assets/Scripts/PauseMenu.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -28,10 +27,30 @@ public class PauseMenu : MonoBehaviour
 		PauseCanvas.SetActive(true);
 	}
 
+	// Starts the timer again and hides the pause menu overlay.
 	public void Resume()
 	{
 		Paused = false;
 		GetComponent<Timer>().enabled = true;
 		PauseCanvas.SetActive(false);
+	}
+
+	// Restarts the currently loaded scene.
+	public void Restart()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+	// Loads the MainMenu scene.
+	public void MainMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	// Loads the Options scene after saving the current's name to PlayerPrefs.
+	public void Options()
+	{
+		PlayerPrefs.SetString("LastScene", SceneManager.GetActiveScene().name);
+		SceneManager.LoadScene("Options");
 	}
 }
