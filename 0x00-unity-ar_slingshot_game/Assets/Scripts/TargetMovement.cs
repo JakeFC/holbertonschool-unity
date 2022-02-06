fd0e1bfd14af52bb6a3f8ce22999beff7af1ecf9
JakeFC <jacobchavera@yahoo.com>
2022-02-06 04:38:32 -0800

    Reverted to using vertex list for spawning and pathing of targets

--- 0x00-unity-ar_slingshot_game/Assets/Scripts/TargetMovement.cs
+++ 0x00-unity-ar_slingshot_game/Assets/Scripts/TargetMovement.cs
@@ -1,14 +1,10 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.UI;
 
 public class TargetMovement : MonoBehaviour
 {
 	private NavMeshAgent _target;
-	//private Vector3[] _verticeList;
+	private Vector3[] _verticeList;
 	private System.Random _rd = new System.Random();
 	private int _randNum = 4, _oppositeFromLast, _last = -1;
 	private float _time = 0;
@@ -18,117 +14,117 @@ public class TargetMovement : MonoBehaviour
     {
         _target = GetComponent<NavMeshAgent>();
 
-		//// List of points on the plane are taken from the parent plane.
-		//_verticeList = transform.parent.GetComponent<TargetSpawning>().verticeList;
+		// List of points on the plane are taken from the parent plane.
+		_verticeList = transform.parent.GetComponent<TargetSpawning>().verticeList;
     }
 
     void Update()
     {
 		_time += Time.deltaTime;
 
-		// Direction updates every 1.3 seconds and 3.2 seconds.
-		if (_time % .8f < 0.1f || _time % 2 < 0.1f)
+		// Direction updates every 1.3 seconds and 4.2 seconds.
+		if (_time % 1.3f < 0.1f || _time % 4.2f < 0.1f)
 			RandomMove();
     }
 
-	// Moves the target in a random direction.
+	// Moves the target toward a random inner vertex.
 	void RandomMove()
 	{
-		//// Top and bottom rows are excluded here.
-		//_randNum = _rd.Next(12, 108);
+		// Top and bottom rows are excluded here.
+		_randNum = _rd.Next(12, 108);
 
-		//// Left and right columns are excluded here.
-		//while(_randNum % 11 == 0 || _randNum % 11 == 10 || _randNum == _last)
-		//	_randNum = _rd.Next(12, 108);
+		// Left and right columns are excluded here, as well as duplicates from last move.
+		while(_randNum % 11 == 0 || _randNum % 11 == 10 || _randNum == _last)
+			_randNum = _rd.Next(12, 108);
 		
-		//_target.destination = _verticeList[_randNum];
+		_target.destination = _verticeList[_randNum];
 
-		_pos = transform.position;
-		_randNum = _rd.Next(0, 7);
+		//_pos = transform.position;
+		//_randNum = _rd.Next(0, 7);
 
-		// Randomize until the number doesn't match the last one.
-		while(_randNum == _last)
-			_randNum = _rd.Next(0, 7);
+		//// Randomize until the number doesn't match the last one.
+		//while(_randNum == _last)
+		//	_randNum = _rd.Next(0, 7);
 		
-		// Each number corresponds to one of eight directions from origin.
-		switch(_randNum)
-		{
-			case 0:
-				_target.destination = new Vector3(_pos.x, _pos.y, _pos.z + .2f);
-				break;
-			case 1:
-				_target.destination = new Vector3(_pos.x + .1f, _pos.y, _pos.z + .1f);
-				break;
-			case 2:
-				_target.destination = new Vector3(_pos.x + .2f, _pos.y, _pos.z);
-				break;
-			case 3:
-				_target.destination = new Vector3(_pos.x + .1f, _pos.y, _pos.z - .1f);
-				break;
-			case 4:
-				_target.destination = new Vector3(_pos.x, _pos.y, _pos.z - .2f);
-				break;
-			case 5:
-				_target.destination = new Vector3(_pos.x - .1f, _pos.y, _pos.z - .1f);
-				break;
-			case 6:
-				_target.destination = new Vector3(_pos.x - .2f, _pos.y, _pos.z);
-				break;
-			case 7:
-				_target.destination = new Vector3(_pos.x - .1f, _pos.y, _pos.z + .1f);
-				break;
-		}
+		//// Each number corresponds to one of eight directions from origin.
+		//switch(_randNum)
+		//{
+		//	case 0:
+		//		_target.destination = new Vector3(_pos.x, _pos.y, _pos.z + .04f);
+		//		break;
+		//	case 1:
+		//		_target.destination = new Vector3(_pos.x + .02f, _pos.y, _pos.z + .02f);
+		//		break;
+		//	case 2:
+		//		_target.destination = new Vector3(_pos.x + .04f, _pos.y, _pos.z);
+		//		break;
+		//	case 3:
+		//		_target.destination = new Vector3(_pos.x + .02f, _pos.y, _pos.z - .02f);
+		//		break;
+		//	case 4:
+		//		_target.destination = new Vector3(_pos.x, _pos.y, _pos.z - .04f);
+		//		break;
+		//	case 5:
+		//		_target.destination = new Vector3(_pos.x - .02f, _pos.y, _pos.z - .02f);
+		//		break;
+		//	case 6:
+		//		_target.destination = new Vector3(_pos.x - .04f, _pos.y, _pos.z);
+		//		break;
+		//	case 7:
+		//		_target.destination = new Vector3(_pos.x - .02f, _pos.y, _pos.z + .02f);
+		//		break;
+		//}
 		_last = _randNum;
 	}
 
 	// Moves the target in the opposite of last direction.
 	void MoveAway()
 	{
-		///// Finds the opposite vertex from previous move.
-		//if (_last != -1)
-		//	_oppositeFromLast = 120 - _last;
-		//else
-		//	_oppositeFromLast = 120 - _randNum;
-		//_target.destination = _verticeList[_oppositeFromLast];
-		//_last = _oppositeFromLast;
-
-		if (_last == -1)
-			_last = _randNum;
-		
-		// Each direction corresponds to the opposite of those in RandomMove()
-		switch(_last)
-		{
-			case 4:
-				_target.destination = new Vector3(_pos.x, _pos.y, _pos.z + .2f);
-				break;
-			case 5:
-				_target.destination = new Vector3(_pos.x + .1f, _pos.y, _pos.z + .1f);
-				break;
-			case 6:
-				_target.destination = new Vector3(_pos.x + .2f, _pos.y, _pos.z);
-				break;
-			case 7:
-				_target.destination = new Vector3(_pos.x + .1f, _pos.y, _pos.z - .1f);
-				break;
-			case 0:
-				_target.destination = new Vector3(_pos.x, _pos.y, _pos.z - .2f);
-				break;
-			case 1:
-				_target.destination = new Vector3(_pos.x - .1f, _pos.y, _pos.z - .1f);
-				break;
-			case 2:
-				_target.destination = new Vector3(_pos.x - .2f, _pos.y, _pos.z);
-				break;
-			case 3:
-				_target.destination = new Vector3(_pos.x - .1f, _pos.y, _pos.z + .1f);
-				break;
-		}
-
-		// _last variable is updated to the opposite _randNum value
-		if (_last > 3)
-			_last -= 4;
+		// Finds the opposite vertex from previous move.
+		if (_last != -1)
+			_oppositeFromLast = 120 - _last;
 		else
-			_last += 4;
+			_oppositeFromLast = 120 - _randNum;
+		_target.destination = _verticeList[_oppositeFromLast];
+		_last = _oppositeFromLast;
+
+		//if (_last == -1)
+		//	_last = _randNum;
+		
+		//// Each direction corresponds to the opposite of those in RandomMove()
+		//switch(_last)
+		//{
+		//	case 4:
+		//		_target.destination = new Vector3(_pos.x, _pos.y, _pos.z + .04f);
+		//		break;
+		//	case 5:
+		//		_target.destination = new Vector3(_pos.x + .02f, _pos.y, _pos.z + .02f);
+		//		break;
+		//	case 6:
+		//		_target.destination = new Vector3(_pos.x + .04f, _pos.y, _pos.z);
+		//		break;
+		//	case 7:
+		//		_target.destination = new Vector3(_pos.x + .02f, _pos.y, _pos.z - .02f);
+		//		break;
+		//	case 0:
+		//		_target.destination = new Vector3(_pos.x, _pos.y, _pos.z - .04f);
+		//		break;
+		//	case 1:
+		//		_target.destination = new Vector3(_pos.x - .02f, _pos.y, _pos.z - .02f);
+		//		break;
+		//	case 2:
+		//		_target.destination = new Vector3(_pos.x - .04f, _pos.y, _pos.z);
+		//		break;
+		//	case 3:
+		//		_target.destination = new Vector3(_pos.x - .02f, _pos.y, _pos.z + .02f);
+		//		break;
+		//}
+
+		//// _last variable is updated to the opposite _randNum value
+		//if (_last > 3)
+		//	_last -= 4;
+		//else
+		//	_last += 4;
 	}
 
 	void OnCollisionEnter(Collision other)
--- 0x00-unity-ar_slingshot_game/Assets/Scripts/TargetSpawning.cs
+++ 0x00-unity-ar_slingshot_game/Assets/Scripts/TargetSpawning.cs
@@ -1,23 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.UI;
 
 public class TargetSpawning : MonoBehaviour
 {
 	public int targetNumber = 5;
 	public int targetsMade = 0;
 	public GameObject target;
-	//public Vector3[] verticeList;
+	public Vector3[] verticeList;
 	private System.Random _rd = new System.Random();
 	private int _randNum;
 	private float _lastSpawnTime = -1;
 
     void Start()
     {
-		//// Saves the list of 121 vertices for the plane.
-        //verticeList = gameObject.GetComponent<MeshFilter>().sharedMesh.vertices;
+		// Saves the list of 121 vertices for the plane.
+        verticeList = gameObject.GetComponent<MeshFilter>().sharedMesh.vertices;
+
+		// Manually updates the vertice list by the plane's position in world space,
+		// since the list is otherwise based on local position.
+		for (int i = 0; i < 120; i++)
+		{
+			verticeList[i] = new Vector3(verticeList[i].x + transform.position.x,
+										verticeList[i].y + transform.position.y,
+										verticeList[i].z + transform.position.z);
+			Debug.Log(verticeList[i].ToString());
+		}
 
 		// Adds a NavMesh component if the plane doesn't have one.
 		if (gameObject.GetComponent<NavMeshSurface>() == null)
@@ -46,23 +53,21 @@ public class TargetSpawning : MonoBehaviour
 	// Attemps to spawn a target on one of the plane's inner vertices.
 	void SpawnTarget()
 	{
-		//// Top and bottom rows are excluded here.
-		//_randNum = _rd.Next(12, 108);
+		// Top and bottom rows are excluded here.
+		_randNum = _rd.Next(12, 108);
 
-		//// Left and right columns are excluded here.
-		//while(_randNum % 11 == 0 || _randNum % 11 == 10)
-		//	_randNum = _rd.Next(12, 108);
+		// Left and right columns are excluded here.
+		while(_randNum % 11 == 0 || _randNum % 11 == 10)
+			_randNum = _rd.Next(12, 108);
 
-		//// 0.733 added to height of the target so it appears on top of the plane.
-		//// The plane is set as a parent object.
-		//Instantiate(target, new Vector3(verticeList[_randNum].x, verticeList[_randNum].y + 0.733f,
-		//			verticeList[_randNum].z), new Quaternion(0, 0, 0, 1), transform);
+		// 0.053 added to height of the target so it appears on top of the plane.
+		// The plane is set as a parent object.
+		Instantiate(target, new Vector3(verticeList[_randNum].x, verticeList[_randNum].y + 0.053f,
+					verticeList[_randNum].z), new Quaternion(0, 0, 0, 1), transform);
 
-		// Spawns a target slightly above the center of the plane with plane as parent.
-		Instantiate(target, new Vector3(transform.position.x, transform.position.y + 0.053f,
-					transform.position.z), new Quaternion(0, 0, 0, 1), transform);
-
-		//Instantiate(target, transform, false);
+		//// Spawns a target slightly above the center of the plane with plane as parent.
+		//Instantiate(target, new Vector3(transform.position.x, transform.position.y + 0.053f,
+		//			transform.position.z), new Quaternion(0, 0, 0, 1), transform);
 
 		targetsMade++;
 	}
