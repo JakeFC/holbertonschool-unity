37677926c5080a343d047ba87cd2ad0b373ea176
JakeFC <jacobchavera@yahoo.com>
2022-02-06 16:15:20 -0800

    Switched from nav mesh movement to Vector3.MoveTowards

--- 0x00-unity-ar_slingshot_game/Assets/Scripts/TargetMovement.cs
+++ 0x00-unity-ar_slingshot_game/Assets/Scripts/TargetMovement.cs
@@ -1,19 +1,20 @@
 using UnityEngine;
-using UnityEngine.AI;
 using UnityEngine.UI;
 
 public class TargetMovement : MonoBehaviour
 {
-	private NavMeshAgent _target;
+	public float speed = 1;
+	//private NavMeshAgent _target;
+	private Vector3 _destination;
 	private Vector3[] _verticeList;
 	private System.Random _rd = new System.Random();
 	private int _randNum = 0, _oppositeFromLast, _last = -1, _numVertices;
 	private float _time = 0;
-	private Vector3 _pos;
+	//private Vector3 _pos;
 
     void Start()
     {
-        _target = GetComponent<NavMeshAgent>();
+        //_target = GetComponent<NavMeshAgent>();
 
 		// List of points on the plane are taken from the parent plane.
 		_verticeList = transform.parent.GetComponent<TargetSpawning>().verticeList;
@@ -27,16 +28,18 @@ public class TargetMovement : MonoBehaviour
 		_time += Time.deltaTime;
 
 		// Direction updates every 1.3 seconds and 4.2 seconds.
-		if (_time % 1.3f < 0.1f || _time % 4.2f < 0.1f)
-			RandomMove();
+		if (_time % 2.6f < 0.1f || _time % 8.4f < 0.1f)
+			SetRandomMove();
+
+		transform.position = Vector3.MoveTowards(transform.position, _destination, speed * Time.deltaTime);
 
 		GameObject.FindWithTag("Debug").GetComponent<Text>().text =
 		(transform.parent.position.ToString() + "\n" +
 		transform.position.ToString());
     }
 
-	// Moves the target toward a random inner vertex.
-	void RandomMove()
+	// Sets the target's move destination to a random inner vertex.
+	void SetRandomMove()
 	{
 		// A random vertex is chosen from the list.
 		_randNum = _rd.Next(0, _numVertices - 1);
@@ -53,7 +56,7 @@ public class TargetMovement : MonoBehaviour
 		//	_randNum = _rd.Next(12, 108);
 		
 		// Add parent position and spawn height difference to convert from local to worldspace.
-		_target.destination = _verticeList[_randNum] + transform.parent.position + new Vector3(0, 0.1f,0);
+		_destination = _verticeList[_randNum] + transform.parent.position + new Vector3(0, 0.1f,0);
 
 		int i = 0;
 		foreach(Vector3 vertex in _verticeList)
@@ -73,34 +76,34 @@ public class TargetMovement : MonoBehaviour
 		//switch(_randNum)
 		//{
 		//	case 0:
-		//		_target.destination = new Vector3(_pos.x, _pos.y, _pos.z + .04f);
+		//		_destination.destination = new Vector3(_pos.x, _pos.y, _pos.z + .04f);
 		//		break;
 		//	case 1:
-		//		_target.destination = new Vector3(_pos.x + .02f, _pos.y, _pos.z + .02f);
+		//		_destination.destination = new Vector3(_pos.x + .02f, _pos.y, _pos.z + .02f);
 		//		break;
 		//	case 2:
-		//		_target.destination = new Vector3(_pos.x + .04f, _pos.y, _pos.z);
+		//		_destination.destination = new Vector3(_pos.x + .04f, _pos.y, _pos.z);
 		//		break;
 		//	case 3:
-		//		_target.destination = new Vector3(_pos.x + .02f, _pos.y, _pos.z - .02f);
+		//		_destination.destination = new Vector3(_pos.x + .02f, _pos.y, _pos.z - .02f);
 		//		break;
 		//	case 4:
-		//		_target.destination = new Vector3(_pos.x, _pos.y, _pos.z - .04f);
+		//		_destination.destination = new Vector3(_pos.x, _pos.y, _pos.z - .04f);
 		//		break;
 		//	case 5:
-		//		_target.destination = new Vector3(_pos.x - .02f, _pos.y, _pos.z - .02f);
+		//		_destination.destination = new Vector3(_pos.x - .02f, _pos.y, _pos.z - .02f);
 		//		break;
 		//	case 6:
-		//		_target.destination = new Vector3(_pos.x - .04f, _pos.y, _pos.z);
+		//		_destination.destination = new Vector3(_pos.x - .04f, _pos.y, _pos.z);
 		//		break;
 		//	case 7:
-		//		_target.destination = new Vector3(_pos.x - .02f, _pos.y, _pos.z + .02f);
+		//		_destination.destination = new Vector3(_pos.x - .02f, _pos.y, _pos.z + .02f);
 		//		break;
 		//}
 		_last = _randNum;
 	}
 
-	// Moves the target in the opposite of last direction.
+	// Sets the target's move destination to the opposite of last destination.
 	void MoveAway()
 	{
 		// Finds the opposite vertex from previous move.
@@ -110,38 +113,38 @@ public class TargetMovement : MonoBehaviour
 			_oppositeFromLast = _numVertices - _randNum;
 
 		// Add parent position to convert from local to worldspace.
-		_target.destination = _verticeList[_oppositeFromLast] + transform.parent.position;
+		_destination = _verticeList[_oppositeFromLast] + transform.parent.position;
 		_last = _oppositeFromLast;
 
 		//if (_last == -1)
 		//	_last = _randNum;
 		
-		//// Each direction corresponds to the opposite of those in RandomMove()
+		//// Each direction corresponds to the opposite of those in SetRandomMove()
 		//switch(_last)
 		//{
 		//	case 4:
-		//		_target.destination = new Vector3(_pos.x, _pos.y, _pos.z + .04f);
+		//		_destination.destination = new Vector3(_pos.x, _pos.y, _pos.z + .04f);
 		//		break;
 		//	case 5:
-		//		_target.destination = new Vector3(_pos.x + .02f, _pos.y, _pos.z + .02f);
+		//		_destination.destination = new Vector3(_pos.x + .02f, _pos.y, _pos.z + .02f);
 		//		break;
 		//	case 6:
-		//		_target.destination = new Vector3(_pos.x + .04f, _pos.y, _pos.z);
+		//		_destination.destination = new Vector3(_pos.x + .04f, _pos.y, _pos.z);
 		//		break;
 		//	case 7:
-		//		_target.destination = new Vector3(_pos.x + .02f, _pos.y, _pos.z - .02f);
+		//		_destination.destination = new Vector3(_pos.x + .02f, _pos.y, _pos.z - .02f);
 		//		break;
 		//	case 0:
-		//		_target.destination = new Vector3(_pos.x, _pos.y, _pos.z - .04f);
+		//		_destination.destination = new Vector3(_pos.x, _pos.y, _pos.z - .04f);
 		//		break;
 		//	case 1:
-		//		_target.destination = new Vector3(_pos.x - .02f, _pos.y, _pos.z - .02f);
+		//		_destination.destination = new Vector3(_pos.x - .02f, _pos.y, _pos.z - .02f);
 		//		break;
 		//	case 2:
-		//		_target.destination = new Vector3(_pos.x - .04f, _pos.y, _pos.z);
+		//		_destination.destination = new Vector3(_pos.x - .04f, _pos.y, _pos.z);
 		//		break;
 		//	case 3:
-		//		_target.destination = new Vector3(_pos.x - .02f, _pos.y, _pos.z + .02f);
+		//		_destination.destination = new Vector3(_pos.x - .02f, _pos.y, _pos.z + .02f);
 		//		break;
 		//}
 
