0b88e814480b16a3c91efa31b7650ced3a1abe91
JakeFC <jacobchavera@yahoo.com>
2022-02-08 10:15:55 -0800

    Fixed quit button script and removed other one. Fixed android build settings

--- 0x00-unity-ar_slingshot_game/Assets/Scripts/QuitButtonBehavior.cs
+++ 0x00-unity-ar_slingshot_game/Assets/Scripts/QuitButtonBehavior.cs
@@ -2,15 +2,9 @@ using UnityEngine;
 
 public class QuitButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Exits the application.
+    public void Quit()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Application.Quit();
     }
 }
