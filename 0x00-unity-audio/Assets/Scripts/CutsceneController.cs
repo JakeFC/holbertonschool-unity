e94b38abcaaa49e3776ee1501df61750821f9c39
Jacob Chavera <77322316+JakeFC@users.noreply.github.com>
2022-03-11 18:52:41 -0800

    Changed indentation to match project

--- 0x00-unity-audio/Assets/Scripts/CutsceneController.cs
+++ 0x00-unity-audio/Assets/Scripts/CutsceneController.cs
@@ -5,16 +5,16 @@ using UnityEngine.UI;
 
 public class CutsceneController : MonoBehaviour
 {
-    public GameObject player;
-    public GameObject MainCamera;
-    public GameObject TimerCanvas;
+	public GameObject player;
+	public GameObject MainCamera;
+	public GameObject TimerCanvas;
     
-    // Enables player movement and timer start when called.
-    void EndCutscene()
-    {
-	player.GetComponent<PlayerController>().enabled = true;
-	MainCamera.SetActive(true);
-	TimerCanvas.SetActive(true);
-	transform.gameObject.SetActive(false);
-    }
+	// Enables player movement and timer start when called.
+	void EndCutscene()
+	{
+		player.GetComponent<PlayerController>().enabled = true;
+		MainCamera.SetActive(true);
+		TimerCanvas.SetActive(true);
+		transform.gameObject.SetActive(false);
+	}
 }
