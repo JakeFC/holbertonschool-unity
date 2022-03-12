7929e2de72978946f0e2f01e5cc260dd4faa10b1
Jacob Chavera <77322316+JakeFC@users.noreply.github.com>
2022-03-11 19:50:37 -0800

    Added comments and fixed indents

--- 0x00-unity-ar_slingshot_game/Assets/Scripts/AmmoCollision.cs
+++ 0x00-unity-ar_slingshot_game/Assets/Scripts/AmmoCollision.cs
@@ -5,25 +5,30 @@ public class AmmoCollision : MonoBehaviour
 {
 	public GameObject ammo;
 	public Text scoreText;
+	// The AmmoMovement script
 	private AmmoMovement _ammoMovement;
 	private GameObject _floor;
 
 	void Start()
 	{
-    	_ammoMovement = ammo.GetComponent<AmmoMovement>();
+    		_ammoMovement = ammo.GetComponent<AmmoMovement>();
 
 		_floor = GameObject.FindWithTag("Floor");
 	}
-    void Update()
-    {
-        if (transform.position.y < _floor.transform.position.y && _ammoMovement.fired)
+
+    	void Update()
+    	{
+		// Reset the ball if it goes below the plane after being fired
+        	if (transform.position.y < _floor.transform.position.y && _ammoMovement.fired)
 			_ammoMovement.ResetBall();
-    }
+   	}
 
 	void OnCollisionEnter(Collision other)
 	{
+		// Ignore collision if not fired yet.
 		if (_ammoMovement.fired == false)
 			return;
+		// Add 10 points if ball hits a target
 		if (other.gameObject.CompareTag("Target"))
 			scoreText.text = (int.Parse(scoreText.text) + 10).ToString();
 		_ammoMovement.ResetBall();
